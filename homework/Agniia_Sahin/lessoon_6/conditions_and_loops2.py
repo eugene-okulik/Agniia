dc548b5e89e2490b4198cd6fdc520231aeaa16b3
Agniia Şahin <agata9595m@mai.ru>
2024-02-15 15:00:24 +0300

    Files from lesson 6

---
+++ homework/Agniia_Sahin/lessoon_6/conditions_and_loops2.py
@@ -0,0 +1,10 @@
+numbers = range(1, 101)
+for number in numbers:
+    if number % 3 == 0:
+        print('fuzz')
+    elif number % 5 == 0:
+        print('buzz')
+    elif number % 3 == 0 and number // 5 == 0:
+        print('FuzzBuzz')
+    else:
+        print(number)
