fa80063b393f873ab96473c6ef336f441ef07709
Agniia Şahin <agata9595m@mai.ru>
2024-02-17 01:03:29 +0300

    changes made on if statement

--- homework/Agniia_Sahin/lessoon_6/conditions_and_loops2.py
+++ homework/Agniia_Sahin/lessoon_6/conditions_and_loops2.py
@@ -1,11 +1,11 @@
 numbers = range(1, 101)
 
 for number in numbers:
-    if number % 3 == 0:
-        print('fuzz')
+    if number % 3 == 0 and number % 5 == 0:
+        print("FuzzBuzz")
+    elif number % 3 == 0:
+        print("fuzz")
     elif number % 5 == 0:
-        print('buzz')
-    elif number % 3 == 0 and number // 5 == 0:
-        print('FuzzBuzz')
+        print("buzz")
     else:
         print(number)
