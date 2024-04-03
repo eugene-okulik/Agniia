fdd5d2452c326d1801d81aeccff3eeb81679c3c3
Agniia Şahin <agata9595m@mai.ru>
2024-04-03 18:19:59 +0300

    changed Files from lesson_8

--- homework/Agniia_Sahin/lesson_8/fibonacci.py
+++ homework/Agniia_Sahin/lesson_8/fibonacci.py
@@ -1,24 +1,27 @@
-def fibonacci(n):
-    if type(n) is not int or n < 0:
-        print("invalid value")
-        return
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        a, b = 0, 1
-        for i in range(2, n + 1):
-            a, b = b, a + b
-        return b
+import sys
+
+sys.set_int_max_str_digits(50000)
 
 
-number_5 = fibonacci(5)
-number_200 = fibonacci(200)
-number_1000 = fibonacci(1000)
-number_100000 = fibonacci(100000)
+def fibonacci():
+    predposlednee = 0
+    poslednee = 1
+    while True:
+        yield predposlednee
+        old_predposlednee = predposlednee
+        predposlednee = poslednee
+        poslednee += old_predposlednee  
 
-print(number_5)
-print(number_200)
-print(number_1000)
-print(number_100000)
+
+count = 0
+for number in fibonacci():
+    count += 1
+    if count == 5:
+        print(number)
+    elif count == 200:
+        print(number)
+    elif count == 1000:
+        print(number)
+    elif count == 100000:
+        print(number)
+        break        
